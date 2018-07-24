1b5c9185fc21008cd3581205463ad40eb13233b4
Karissa Bui <karissab002@gmail.com>
2018-07-24 21:24:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,15 @@
-puts "Enter in a number"
+puts "Enter an integer"
 int = gets.chomp
 
 def fizzbuzz(int)
   if int % 3 == 0 
-    puts "fizz"
+    puts "Fizz"
+  elsif int % 5 == 0 
+    puts "Buzz"
+  elsif int % 15 == 0 
+    puts "Fizzbuzz"
+  else 
+    puts "Nil"
   end
 end
 
